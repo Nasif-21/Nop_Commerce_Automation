85230adcbe812aed3a43ef4a83866d0251058ce8
Nasif <nasifshitab01@gmail.com>
2026-05-13 13:58:11 +0600

    Added an UserModel to do Register using a Model

---
+++ src/test/java/config/UserModel.java
@@ -0,0 +1,82 @@
+package config;
+
+public class UserModel {
+
+String FirstName;
+String LastName;
+String Email;
+String CompanyName;
+String VatNo;
+String CPF;
+String Password;
+String ConfirmPassword;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getCompanyName() {
+        return CompanyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        CompanyName = companyName;
+    }
+
+    public String getVatNo() {
+        return VatNo;
+    }
+
+    public void setVatNo(String vatNo) {
+        VatNo = vatNo;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getConfirmPassword() {
+        return ConfirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        ConfirmPassword = confirmPassword;
+    }
+
+    public UserModel()
+    {
+
+    }
+}
