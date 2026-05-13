111e925d61f6066b38d4ee804a188f0895c3bbe5
Nasif <nasifshitab01@gmail.com>
2026-05-13 13:56:53 +0600

    Setup the driver and the link to be test

---
+++ src/test/java/config/Setup.java
@@ -0,0 +1,31 @@
+package config;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+public class Setup {
+
+    public WebDriver driver;
+
+    @BeforeTest
+    public void Setup()
+    {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://test470.nop-station.com/");
+    }
+
+    @AfterTest
+    public void tearDown()
+    {
+       // driver.quit();
+
+    }
+}
