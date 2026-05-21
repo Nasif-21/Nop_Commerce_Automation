20cb06cfd21bf96796296124d053e3b58577d249
Nasif <nasifshitab01@gmail.com>
2026-05-21 23:52:16 +0600

    Fixed login page

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,10 +1,22 @@
 package pages;
 
+import Utils.Utils;
+import config.UserModel;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
     //Locators
+    @FindBy(id="Email")
+    WebElement emailtxt;
+
+    @FindBy(id="Password")
+    WebElement passwordtxt;
+
+    @FindBy(className ="login-button")
+    WebElement loginbtn;
 
 
 
@@ -14,5 +26,11 @@ public class LoginPage {
     }
 
     //Methods
+    public void doLogin(String email, String password)
+    {
+        emailtxt.sendKeys(email);
+        passwordtxt.sendKeys(password);
+        loginbtn.click();
+    }
 
 }
