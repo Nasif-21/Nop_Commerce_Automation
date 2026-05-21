c363ef3b92907c4114cae8aeaeaba5a23240fd68
Nasif <nasifshitab01@gmail.com>
2026-05-21 22:28:15 +0600

    Call a method to extract token data from localstorage

--- src/test/java/testRunner/LoginTestRunner.java
+++ src/test/java/testRunner/LoginTestRunner.java
@@ -1,18 +1,39 @@
 package testRunner;
 
+import Utils.Utils;
 import config.Setup;
+import config.UserModel;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
+
+import java.io.IOException;
 
 
 public class LoginTestRunner extends Setup {
 
     @Test
-    public void doLogin()
-    {
+    public void doLogin() throws IOException, ParseException, InterruptedException {
+        String filepath="./src/test/resources/user.json";
+
         HomePage homePage = new HomePage(driver);
         homePage.clickLinkLogin();
+
+        JSONObject jsonObj=Utils.readJsonData(filepath);
+        String email=jsonObj.get("email").toString();
+        String password=jsonObj.get("password").toString();
+
+        LoginPage loginPage=new  LoginPage(driver);
+        loginPage.doLogin(email,password);
+        Thread.sleep(2000);
+        Utils.getAuth(driver);
+
+
+
+
+
 
 
     }
