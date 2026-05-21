d7fe130ed148db61a23a0fcbf16a40bc0e5ce575
Nasif <nasifshitab01@gmail.com>
2026-05-21 23:51:01 +0600

    Added token injection using setAuth

--- src/test/java/testRunner/HomePageTestRunner.java
+++ src/test/java/testRunner/HomePageTestRunner.java
@@ -13,12 +13,13 @@ public class HomePageTestRunner extends Setup {
     @BeforeTest
     public void doLogin() throws IOException, ParseException, InterruptedException {
         Utils.setAuth(driver);
+        driver.get("https://test470.nop-station.com");
     }
 
     @Test
-    public void doPurchase()
-    {
+    public void doPurchase() throws InterruptedException {
         HomePage homePage = new HomePage(driver);
+        Thread.sleep(2000);
         homePage.doPurchase();
     }
 }
