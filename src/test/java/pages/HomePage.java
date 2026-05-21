1014aba1b181dc57093090889b746ceceacfd4c7
Nasif <nasifshitab01@gmail.com>
2026-05-21 23:50:18 +0600

    Fixed issue

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,11 +1,20 @@
 package pages;
 
+import Utils.Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class HomePage {
+    WebDriver driver;
 
     //Locators
     @FindBy(className = "ico-register")
@@ -20,8 +29,19 @@ public class HomePage {
     @FindBy(className = "ico-logout")
     WebElement linkLogout;
 
+    @FindBy(xpath="//a[@href='/electronics']")
+    WebElement linkElectronics;
+
+    @FindBy(xpath = "//a[@href='/cell-phones']")
+    WebElement linkCellPhone;
+
+
+
+
+
     //Initializer
     public HomePage(WebDriver driver) {
+        this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
@@ -40,5 +60,22 @@ public class HomePage {
     {
         linkLogout.click();
     }
+
+    public void doPurchase()
+    {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(linkElectronics).perform();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(linkCellPhone));
+        linkCellPhone.click();
+        /*String pagelink = driver.getCurrentUrl();
+        System.out.println(pagelink);*/
+    }
+
+   /* public String getUrlCart()
+    {
+        doPurchase();
+        return driver.getCurrentUrl();
+    }*/
 
 }
