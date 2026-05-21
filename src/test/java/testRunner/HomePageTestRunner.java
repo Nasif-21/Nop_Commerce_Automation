7634997a8cf30f4834584cb9c110c37c832bc3d1
Nasif <nasifshitab01@gmail.com>
2026-05-21 22:32:37 +0600

    Fixed token injection

--- src/test/java/testRunner/HomePageTestRunner.java
+++ src/test/java/testRunner/HomePageTestRunner.java
@@ -1,10 +1,24 @@
 package testRunner;
 
+import Utils.Utils;
 import config.Setup;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
+import java.io.IOException;
+
 public class HomePageTestRunner extends Setup {
+    @BeforeTest
+    public void doLogin() throws IOException, ParseException, InterruptedException {
+        Utils.setAuth(driver);
+    }
 
-
+    @Test
+    public void doPurchase()
+    {
+        HomePage homePage = new HomePage(driver);
+        homePage.doPurchase();
+    }
 }
