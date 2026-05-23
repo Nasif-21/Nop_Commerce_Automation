cd83c497186cfa91317af0a37f467cfe01ed92eb
Nasif <nasifshitab01@gmail.com>
2026-05-24 00:39:17 +0600

    Added assertions to page errors

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import Utils.Utils;
 import config.UserModel;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,6 +19,12 @@ public class LoginPage {
     @FindBy(className ="login-button")
     WebElement loginbtn;
 
+    @FindBy(xpath = "//span[@id='Email-error']")
+    public WebElement errormsg;
+
+    // Error text inside list
+    @FindBy(xpath = "//div[contains(@class,'validation-summary-errors')]//ul/li")
+    public WebElement errorValid;
 
 
     //Initializer
@@ -31,6 +38,16 @@ public class LoginPage {
         emailtxt.sendKeys(email);
         passwordtxt.sendKeys(password);
         loginbtn.click();
+    }
+
+    public void clearTxtBox()
+    {
+        emailtxt.sendKeys(Keys.CONTROL + "a",Keys.BACK_SPACE);
+        passwordtxt.sendKeys(Keys.CONTROL + "a",Keys.BACK_SPACE);
+    }
+    public void sendinvalidEmail(String email)
+    {
+        emailtxt.sendKeys(email);
     }
 
 }
