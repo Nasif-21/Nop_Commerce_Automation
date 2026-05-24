68a0f75758fbf82e273025c03ca584bef386f014
Nasif <nasifshitab01@gmail.com>
2026-05-24 22:35:19 +0600

    Added carts locator and added simple tests, work ongoing

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,10 +1,29 @@
 package pages;
 
+import Utils.Utils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CartPage {
     WebDriver driver;
+
+
+    @FindBy(className = "no-data")
+    public WebElement txtEmptyCart;
+
+    @FindBy(id="terms-of-service-warning-box")
+    public WebElement txtMsgTermsOfService;
+
+    @FindBy(xpath = "//button[@title='Close' and contains(@class,'ui-dialog-titlebar-close')]")
+    WebElement btnPopupClose;
+
+    @FindBy(id="termsofservice")
+    WebElement checkboxTermsofService;
+
+    @FindBy(id = "checkout")
+    WebElement btnCheckout;
 
 
 
@@ -13,6 +32,24 @@ public class CartPage {
         PageFactory.initElements(driver, this);
 
     }
+
+    public void doCheckoutWithoutTermsofService() throws InterruptedException {
+        Utils.scrollTo(driver,checkboxTermsofService);
+        btnCheckout.click();
+        Thread.sleep(2000);
+    }
+
+    public void closePopup() throws InterruptedException {
+        btnPopupClose.click();
+    }
+
+    public void doCheckout()
+    {
+        Utils.scrollTo(driver,checkboxTermsofService);
+        checkboxTermsofService.click();
+        btnCheckout.click();
+    }
+
 
 
 
--- src/test/java/testRunner/CartTestRunner.java
+++ src/test/java/testRunner/CartTestRunner.java
@@ -3,22 +3,56 @@ package testRunner;
 import Utils.Utils;
 import config.Setup;
 import org.json.simple.parser.ParseException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import pages.CartPage;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class CartTestRunner extends Setup {
 
+    CartPage cartPage;
+    WebDriverWait wait;
+
     @BeforeTest
     public void doLogin() throws IOException, ParseException, InterruptedException {
-        Utils.setAuth(driver);
+        // Check the cart is empty before login
         driver.get("https://test470.nop-station.com/cart");
+        cartPage = new CartPage(driver);
+
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(cartPage.txtEmptyCart));
+
+        String msg=cartPage.txtEmptyCart.getText();
+        Assert.assertEquals(msg,"Your Shopping Cart is empty!");
+
+        Thread.sleep(2000);
+        Utils.setAuth(driver);
+        driver.navigate().refresh();
+
+
     }
 
     @Test(priority = 1,description = "Check if the product added successfully")
     public void checkCart()
     {
+
+
+    }
+
+    @Test(description = "Click the product checkout without accepting terms and services")
+    public void checkTermsAndServices() throws InterruptedException {
+        cartPage.doCheckoutWithoutTermsofService();
+        String msg=cartPage.txtMsgTermsOfService.getText();
+
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(cartPage.txtMsgTermsOfService));
+        Assert.assertEquals(msg,"Please accept the terms of service before the next step.");
+        cartPage.closePopup();
 
     }
 
