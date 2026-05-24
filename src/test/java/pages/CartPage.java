342ebc8be435742952809d44cdbdcf4556fe261f
Nasif <nasifshitab01@gmail.com>
2026-05-24 23:56:48 +0600

    Calculate Cart pricing using key value pair Hashmap

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -6,8 +6,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CartPage {
     WebDriver driver;
+
+    @FindBy(css = "a.product-name")
+    public WebElement txtProductName;
+
+    @FindBy(css = "span.product-unit-price")
+    public WebElement txtUnitPrice;
+
+    @FindBy(css = "input.qty-input")
+    public WebElement txtQuantity;
+
+    @FindBy(css = "span.product-subtotal")
+    public WebElement txtTotalPrice;
+
+    @FindBy(id="checkout_attribute_1")
+    public WebElement giftWrapValue;
+
+    @FindBy(id="checkout_attribute_2_3")
+    public WebElement PackValue;
 
 
     @FindBy(className = "no-data")
@@ -32,6 +53,19 @@ public class CartPage {
         PageFactory.initElements(driver, this);
 
     }
+
+
+    public Map<String,Double> cartDetails()
+    {
+        Map<String,Double> cart=new HashMap<>();
+        cart.put("unitPrice",Utils.NumConverter(txtUnitPrice.getText()));
+        cart.put("quantity",Utils.NumConverter(txtQuantity.getAttribute("value").trim()));
+        cart.put("totalPrice",Utils.NumConverter(txtTotalPrice.getText()));
+
+        return cart;
+
+    }
+
 
     public void doCheckoutWithoutTermsofService() throws InterruptedException {
         Utils.scrollTo(driver,checkboxTermsofService);
