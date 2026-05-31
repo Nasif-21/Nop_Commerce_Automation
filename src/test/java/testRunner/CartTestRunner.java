6e8aeb07c5af14ab5540d23457bbf67166bfd5d9
Nasif <nasifshitab01@gmail.com>
2026-05-31 22:07:10 +0600

    Added test for price calculation and moved to billing address

--- src/test/java/testRunner/CartTestRunner.java
+++ src/test/java/testRunner/CartTestRunner.java
@@ -12,6 +12,8 @@ import pages.CartPage;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CartTestRunner extends Setup {
 
@@ -40,12 +42,59 @@ public class CartTestRunner extends Setup {
     @Test(priority = 1,description = "Check if the product added successfully")
     public void checkCart()
     {
-
+        String productName=cartPage.txtProductName.getText();
+        Assert.assertEquals(productName,"HTC One Mini Blue");
 
     }
 
-    @Test(description = "Click the product checkout without accepting terms and services")
+    @Test(priority = 2, description = "Calculate Price")
+    public void calcPrice()
+    {
+    Utils.scrollTo(driver, cartPage.txtProductName);
+        Map<String,Double> cart=cartPage.cartDetails();
+
+        double expected=cart.get("totalPrice");
+        double actual=cart.get("unitPrice")*cart.get("quantity");
+        Assert.assertEquals(actual,expected);
+
+    }
+
+    @Test(priority = 3, description = "Subtotal value check")
+    public void checkSubtotal() throws InterruptedException {
+
+
+        boolean priceTrue=cartPage.PackValue.isSelected();
+        Map <String,Double> cart=cartPage.cartDetails();
+
+        //Price Calculation
+
+        Thread.sleep(2000);
+        double subtotal=Utils.NumConverter(cartPage.txtSubTotal.getText());
+        double shippingCost=Utils.NumConverter(cartPage.txtShippingCost.getText());
+        double tax=Utils.NumConverter(cartPage.txtTaxValue.getText());
+
+        if(priceTrue)
+        {
+            double finalTotal=cart.get("totalPrice")+100;
+            double total=subtotal+shippingCost+tax;
+
+            Assert.assertEquals(finalTotal,total,"Mismatched cart value, please check");
+
+
+        }
+        else
+        {
+         double finalTotal=cart.get("totalPrice");
+         double total=subtotal+shippingCost+tax;
+         Assert.assertEquals(finalTotal,total,"Mismatched cart value, please check");
+
+        }
+
+    }
+
+    @Test(priority = 4, description = "Click the product checkout without accepting terms and services")
     public void checkTermsAndServices() throws InterruptedException {
+
         cartPage.doCheckoutWithoutTermsofService();
         String msg=cartPage.txtMsgTermsOfService.getText();
 
@@ -56,17 +105,23 @@ public class CartTestRunner extends Setup {
 
     }
 
-    @Test(description ="Check product name" )
-    public void  checkProductName()
+    @Test(priority = 5, description = "Doing checkout")
+    public void properCheckout()
     {
-
+        cartPage.doCheckout();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(driver.getCurrentUrl()));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/billingaddress");
     }
 
-    @Test(priority = 3, description = "Calculate Price")
-    public void calcPrice()
-    {
+//
+//    @Test(description ="Check product name" )
+//    public void  checkProductName()
+//    {
+//
+//    }
+//
 
-    }
 
 
 }
