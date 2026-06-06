61f57404ca602c98a5f6ce3cf836ae5184432567
Nasif <nasifshitab01@gmail.com>
2026-06-06 23:24:01 +0600

    Locators and tests for Shipping methods

--- src/test/java/pages/ShippingMethodPage.java
+++ src/test/java/pages/ShippingMethodPage.java
@@ -1,16 +1,82 @@
 package pages;
 
+import Utils.Utils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ShippingMethodPage {
     WebDriver driver;
+    WebDriverWait wait;
+
+
+//Find locators and do rest of work
+    @FindBy(id = "PickupInStore")
+    WebElement checkboxpickStore;
+
+    @FindBy(id="shippingoption_0")
+    WebElement radioshippingGround;
+
+    @FindBy(id="shippingoption_1")
+    WebElement radioshippingAir;
+
+    @FindBy(id="shippingoption_2")
+    WebElement radioshippingAir2;
+
+    @FindBy(className = "shipping-method-next-step-button")
+    WebElement btnNext;
 
 
 
     public ShippingMethodPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+
+    }
+
+    public void storeShipping()
+    {
+        Utils.scrollTo(driver, btnNext);
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(checkboxpickStore));
+        checkboxpickStore.click();
+        //btnNext.click();
+
+
+    }
+
+    public void groundShipping()
+    {
+        Utils.scrollTo(driver, btnNext);
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(radioshippingGround));
+        radioshippingGround.click();
+        //btnNext.click();
+
+    }
+
+    public void airShipping()
+    {
+        Utils.scrollTo(driver, btnNext);
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(radioshippingAir));
+        radioshippingAir.click();
+        //btnNext.click();
+
+    }
+
+    public void airShipping2()
+    {
+        Utils.scrollTo(driver, btnNext);
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(radioshippingAir2));
+        radioshippingAir2.click();
+        //btnNext.click();
 
     }
 
--- src/test/java/testRunner/ShippingMethodTestRunner.java
+++ src/test/java/testRunner/ShippingMethodTestRunner.java
@@ -3,14 +3,69 @@ package testRunner;
 import Utils.Utils;
 import config.Setup;
 import org.json.simple.parser.ParseException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.ShippingMethodPage;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class ShippingMethodTestRunner extends Setup {
+    WebDriverWait wait;
+    ShippingMethodPage shippingMethodPage;
+
     @BeforeTest
     public void doLogin() throws IOException, ParseException, InterruptedException {
         Utils.setAuth(driver);
         driver.get("https://test470.nop-station.com/checkout/shippingmethod");
     }
+
+    @Test(priority = 1,description = "Shipping using Store")
+    public void StoreShipTest()
+    {
+        shippingMethodPage = new ShippingMethodPage(driver);
+        shippingMethodPage.storeShipping();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(driver.getCurrentUrl()));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/paymentmethod");
+
+    }
+
+    //If anyone wants to check other payment methods, just remove enable=false and in suite.xml, provide suite name to regression
+    @Test(priority = 2,description = "Shipping using ground", suiteName = "regression", enabled = false)
+    public void GroundShipTest()
+    {
+        shippingMethodPage = new ShippingMethodPage(driver);
+        shippingMethodPage.groundShipping();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(driver.getCurrentUrl()));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/paymentmethod");
+
+    }
+
+    @Test(priority = 3,description = "Shipping using Air",suiteName = "regression", enabled = false)
+    public void AirShipTest()
+    {
+        shippingMethodPage = new ShippingMethodPage(driver);
+        shippingMethodPage.airShipping();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(driver.getCurrentUrl()));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/paymentmethod");
+
+    }
+
+    @Test(priority = 4,description = "Shipping using Air 2",suiteName = "regression", enabled = false)
+    public void AirShipTest2()
+    {
+        shippingMethodPage = new ShippingMethodPage(driver);
+        shippingMethodPage.airShipping2();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe(driver.getCurrentUrl()));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/paymentmethod");
+    }
+
+
 }
