4c059272d19fc56bf0e46e864c8c56b06e169ef8
Nasif <nasifshitab01@gmail.com>
2026-06-06 23:25:16 +0600

    Made an address model so that we can use it on billing and shipping address

---
+++ src/test/java/config/AddressModel.java
@@ -0,0 +1,30 @@
+package config;
+
+
+
+public class AddressModel {
+    String city;
+    String address1;
+
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public AddressModel()
+    {}
+
+
+}
