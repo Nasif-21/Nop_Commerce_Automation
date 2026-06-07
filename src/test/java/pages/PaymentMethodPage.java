637f3ae037b1ef0a73ed3238ad02ef518ca2fd89
Nasif <nasifshitab01@gmail.com>
2026-06-07 21:56:23 +0600

    Locator added to the Payment Method page and added tests for various payment conditions inside Payment Method Test Runner

--- src/test/java/pages/PaymentMethodPage.java
+++ src/test/java/pages/PaymentMethodPage.java
@@ -1,10 +1,35 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class PaymentMethodPage {
     WebDriver driver;
+    WebDriverWait wait;
+
+    @FindBy(className = "payment-method-next-step-button")
+    WebElement btnNext;
+
+    @FindBy(id="paymentmethod_0")
+    WebElement radioNetsEasy;
+
+    @FindBy(id="paymentmethod_1")
+    WebElement radioGetNet;
+
+    @FindBy(id="paymentmethod_2")
+    WebElement radioConvergePayment;
+
+    @FindBy(id="paymentmethod_3")
+    WebElement radioCheckorMoneyOrder;
+
+    @FindBy(id="paymentmethod_4")
+    WebElement radioPayFast;
 
 
 
@@ -13,6 +38,48 @@ public class PaymentMethodPage {
         PageFactory.initElements(driver, this);
 
     }
+
+    public void NetsEasyPayment()
+    {
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        radioNetsEasy.click();
+        btnNext.click();
+    }
+
+    public void GetNetPayment()
+    {
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(radioGetNet));
+        radioGetNet.click();
+        btnNext.click();
+    }
+
+    public void ConvergePayment()
+    {
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(radioConvergePayment));
+        radioConvergePayment.click();
+        btnNext.click();
+    }
+
+    public void CheckOrMoneyOrderPayment()
+    {
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(radioCheckorMoneyOrder));
+        radioCheckorMoneyOrder.click();
+        btnNext.click();
+    }
+
+    public void PayFastPayment()
+    {
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(radioPayFast));
+        radioPayFast.click();
+        btnNext.click();
+    }
+
+
+
 
 
 
--- src/test/java/testRunner/PaymentMethodTestRunner.java
+++ src/test/java/testRunner/PaymentMethodTestRunner.java
@@ -3,14 +3,77 @@ package testRunner;
 import Utils.Utils;
 import config.Setup;
 import org.json.simple.parser.ParseException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.PaymentMethodPage;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class PaymentMethodTestRunner extends Setup {
+
+    PaymentMethodPage paymentMethodPage;
+    WebDriverWait wait;
+
     @BeforeTest
     public void doLogin() throws IOException, ParseException, InterruptedException {
         Utils.setAuth(driver);
         driver.get("https://test470.nop-station.com/checkout/paymentmethod");
     }
+
+    // I have used payment using check or money order, if you want to check others, just make the enabled=true
+    @Test(description = "Payment using Nets Easy",enabled = true)
+    public void paymentNetsEasy()
+    {
+        paymentMethodPage = new PaymentMethodPage(driver);
+        paymentMethodPage.NetsEasyPayment();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("https://test470.nop-station.com/checkout/paymentinfo"));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/paymentinfo");
+    }
+
+    @Test(description = "Payment using Get Net",enabled = false)
+    public void paymentGetNet()
+    {
+        paymentMethodPage = new PaymentMethodPage(driver);
+        paymentMethodPage.GetNetPayment();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("https://test470.nop-station.com/checkout/paymentinfo"));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/paymentinfo");
+    }
+
+    @Test(description = "Payment using Converge Payment",enabled = false)
+    public void paymentConverge()
+    {
+        paymentMethodPage = new PaymentMethodPage(driver);
+        paymentMethodPage.ConvergePayment();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("https://test470.nop-station.com/checkout/paymentinfo"));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/paymentinfo");
+    }
+
+    @Test(description = "Payment using Check or Money order",enabled = true)
+    public void paymentCheckorMoney()
+    {
+        paymentMethodPage = new PaymentMethodPage(driver);
+        paymentMethodPage.CheckOrMoneyOrderPayment();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("https://test470.nop-station.com/checkout/paymentinfo"));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/paymentinfo");
+    }
+
+    @Test(description = "Payment using PayFast",enabled = false)
+    public void paymentPayFast()
+    {
+        paymentMethodPage = new PaymentMethodPage(driver);
+        paymentMethodPage.PayFastPayment();
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("https://test470.nop-station.com/checkout/paymentinfo"));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/paymentinfo");
+    }
+
+
 }
