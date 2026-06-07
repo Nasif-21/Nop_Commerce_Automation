bc9eb8a5225166c6ae83600f20ea1076abdaf5bb
Nasif <nasifshitab01@gmail.com>
2026-06-08 00:06:41 +0600

    Added locators and address assertion

--- src/test/java/pages/PaymentInfoPage.java
+++ src/test/java/pages/PaymentInfoPage.java
@@ -1,17 +1,44 @@
 package pages;
 
+import Utils.Utils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class PaymentInfoPage {
     WebDriver driver;
+    WebDriverWait wait;
 
+
+    @FindBy(className="payment-info-next-step-button")
+    WebElement nextStepButton;
+
+    @FindBy(css = "div.info")
+    public WebElement txtInformation;
+
+    @FindBy(xpath = "//div[@class='info']//b[contains(text(),'NOP SOLUTIONS')]")
+    public WebElement txtCompanyName;
+
+    @FindBy(xpath = "//div[@class='info']//b[contains(text(),'New York')]")
+    public WebElement txtCityAddress;
+
+    @FindBy(xpath="//div[@class='info']//b[contains(text(),'USA')]")
+    public WebElement txtCountryName;
 
 
     public PaymentInfoPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
 
+    }
+
+    public void clicknextStepButton(){
+        nextStepButton.click();
     }
 
 
--- src/test/java/testRunner/PaymentInfoTestRunner.java
+++ src/test/java/testRunner/PaymentInfoTestRunner.java
@@ -3,14 +3,53 @@ package testRunner;
 import Utils.Utils;
 import config.Setup;
 import org.json.simple.parser.ParseException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.PaymentInfoPage;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class PaymentInfoTestRunner extends Setup {
+
+    PaymentInfoPage paymentInfoPage;
+    WebDriverWait wait;
+
     @BeforeTest
     public void doLogin() throws IOException, ParseException, InterruptedException {
         Utils.setAuth(driver);
         driver.get("https://test470.nop-station.com/checkout/paymentinfo");
+    }
+
+    @Test(priority = 1, description = "Completing payment using payment gateway")
+    public void finishPayment() throws InterruptedException {
+        paymentInfoPage = new PaymentInfoPage(driver);
+        wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.visibilityOf(paymentInfoPage.txtInformation));
+
+        Utils.scrollTo(driver, paymentInfoPage.txtInformation);
+
+        String companyName = paymentInfoPage.txtCompanyName.getText();
+        String cityAddress = paymentInfoPage.txtCityAddress.getText();
+        String countryName = paymentInfoPage.txtCountryName.getText();
+
+
+        Assert.assertEquals(companyName, "NOP SOLUTIONS");
+        Assert.assertEquals(cityAddress,"New York, NY 10001");
+        Assert.assertEquals(countryName,"USA");
+
+        Thread.sleep(2000);
+
+        /*paymentInfoPage.clicknextStepButton();
+
+        wait.until(ExpectedConditions.urlToBe(driver.getCurrentUrl()));
+        Assert.assertEquals(driver.getCurrentUrl(),"https://test470.nop-station.com/checkout/confirm");*/
+
+
+
     }
 }
