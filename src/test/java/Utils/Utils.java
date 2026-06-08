5ed89843977e3b2d5a0a2508a0607a85dd59785e
Nasif <nasifshitab01@gmail.com>
2026-06-09 01:39:45 +0600

    Added a user session cookies method

--- src/test/java/Utils/Utils.java
+++ src/test/java/Utils/Utils.java
@@ -133,4 +133,109 @@ public class Utils {
     {
         return Double.parseDouble(pricetxt.replace("$","").replace(",","").trim());
     }
+
+   /* public static void getCustomerAuth(WebDriver driver) throws IOException, ParseException {
+
+        Cookie customerCookie = driver.manage().getCookieNamed("Nop.customer");
+
+        if (customerCookie != null) {
+            String customerToken = customerCookie.getValue();
+
+            // ✅ Read existing JSON file first
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject;
+
+            try {
+                jsonObject = (JSONObject) jsonParser.parse(new FileReader("./src/test/resources/localstorage.json"));
+            } catch (Exception e) {
+                jsonObject = new JSONObject();
+            }
+
+
+            jsonObject.put("Nop.customer", customerToken);
+
+
+            FileWriter writer = new FileWriter("./src/test/resources/localstorage2.json");
+            writer.write(jsonObject.toJSONString());
+            writer.flush();
+            writer.close();
+
+        } else {
+            System.out.println("No customer session found, check code");
+        }
+    }
+
+
+    // Inject auth token into browser
+    public static void setCustomerAuth(WebDriver driver) throws IOException, ParseException, InterruptedException {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject authObj = (JSONObject) jsonParser.parse(new FileReader("./src/test/resources/localstorage2.json"));
+
+        // ✅ Use same key name used in getCustomerAuth()
+        String customerToken = authObj.get("Nop.customer").toString();
+
+        String currentUrl = driver.getCurrentUrl();
+        String domain = new URL(currentUrl).getHost();
+
+        Cookie customerCookie = new Cookie.Builder("Nop.customer", customerToken)
+                .domain(domain)
+                .path("/")
+                .isSecure(true)
+                .isHttpOnly(true)
+                .build();
+
+        driver.manage().addCookie(customerCookie);
+
+        Thread.sleep(2000);
+    }*/
+
+    public static void getCustomerAuth(WebDriver driver) throws IOException {
+
+        // As the auth token saved in cookies, we need to get cookies using sellenium cookies method
+
+        Cookie authCookie = driver.manage().getCookieNamed(".Nop.Customer");
+
+        if (authCookie != null) {
+            String authToken=authCookie.getValue();
+            JSONObject jsonObject=new JSONObject();
+            jsonObject.put(".Nop.Customer",authToken);
+            FileWriter writer=new FileWriter("./src/test/resources/localstorage2.json");
+            writer.write(jsonObject.toJSONString());
+            writer.flush();
+            writer.close();
+
+        }
+        else  {
+            System.out.println("No customer session found, check code");
+        }
+
+
+
+    }
+
+
+    // Inject auth token into browser
+    public static void setCustomerAuth(WebDriver driver) throws IOException, ParseException, InterruptedException {
+        JSONParser jsonParser=new JSONParser();
+        JSONObject authObj= (JSONObject) jsonParser.parse(new FileReader("./src/test/resources/localstorage2.json"));
+        String authToken=authObj.get(".Nop.Customer").toString();
+        String currentUrl= driver.getCurrentUrl();
+        String domain=new URL(currentUrl).getHost();
+
+        Cookie authCookie = new Cookie.Builder(".Nop.Customer",authToken)
+                .domain(domain)
+                .path("/")
+                .isSecure(true)
+                .isHttpOnly(true)
+                .build();
+
+        driver.manage().addCookie(authCookie);
+
+
+
+        Thread.sleep(2000);
+
+    }
+
+
 }
